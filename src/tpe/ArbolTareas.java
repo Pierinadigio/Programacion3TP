5d4342df80de82f6d1fe7534c699eff85982b2bf
Pierinadigio <pierinadigiorgio@gmail.com>
2024-06-28 19:58:39 -0300

    cambios

---
+++ src/tpe/ArbolTareas.java
@@ -0,0 +1,73 @@
+package tpe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArbolTareas {
+    private Nodo root;
+
+    public ArbolTareas() {
+        this.root = null;
+    }
+
+    public void insert(MyTask task) {
+        root = insertRec(root, task);
+    }
+
+    private Nodo insertRec(Nodo root, MyTask task) {
+        if (root == null) {
+            root = new Nodo(task.getNivelPrioridad());
+            root.addTask(task);
+            return root;
+        }
+
+        if (task.getNivelPrioridad() < root.priority) {
+            root.left = insertRec(root.left, task);
+        } else if (task.getNivelPrioridad() > root.priority) {
+            root.right = insertRec(root.right, task);
+        } else {
+            root.addTask(task);  
+        }
+
+        return root;
+    }
+
+    public List<MyTask> getTasksInPriorityRange(int low, int high) {
+        List<MyTask> result = new ArrayList<>();
+        getTasksInPriorityRangeRec(root, low, high, result);
+        return result;
+    }
+
+    private void getTasksInPriorityRangeRec(Nodo root, int low, int high, List<MyTask> result) {
+        if (root == null) {
+            return;
+        }
+
+        if (low < root.priority) {
+            getTasksInPriorityRangeRec(root.left, low, high, result);
+        }
+
+        if (low <= root.priority && high >= root.priority) {
+            result.addAll(root.tasks);
+        }
+
+        if (high > root.priority) {
+            getTasksInPriorityRangeRec(root.right, low, high, result);
+        }
+    }
+    
+    public List<MyTask> getTasksInPriorityOrder() {
+        List<MyTask> result = new ArrayList<>();
+        getTasksInPriorityOrderRec(root, result);
+        return result;
+    }
+
+    private void getTasksInPriorityOrderRec(Nodo node, List<MyTask> result) {
+        if (node != null) {
+            getTasksInPriorityOrderRec(node.right, result); 
+            result.addAll(node.getTasks()); 
+            getTasksInPriorityOrderRec(node.left, result); 
+            
+        }
+    }
+}
---
+++ src/tpe/Main.java
@@ -0,0 +1,58 @@
+package tpe;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    //----------------------PRIMERA PARTE TP---------------------------------------------------------------//	
+       Servicios serv = new Servicios ("./src/tpe/datasets/Procesadores.csv","./src/tpe/datasets/Tareas.csv" );
+      
+       //Servicio1
+       MyTask tarea5 = serv.servicio1("T5");
+       System.out.println("\nServicio1:   Tarea:  " + datosTarea(tarea5));
+   
+       
+       //Servicio2
+      List<MyTask> lista = serv.servicio2(true);
+      System.out.println("\nServicio2: Lista de tareas Criticas/No Criticas\n" + listadoTareas(lista));
+      
+      //Servicio3
+      List<MyTask> tareasPorPrioridad = serv.servicio3(50,80);
+      System.out.println("\nServicio3: Tareas por PRIORIDAD \n" + listadoTareas(tareasPorPrioridad));
+     
+      
+    //----------------------PRIMERA PARTE TP---------------------------------------------------------------//
+      
+      //Servicio4
+      int tiempoMaxNoRefrigerado = 200;
+      System.out.println("\nServicio 4: Backtracking: Solucion Obtenida");
+      serv.servicio4(tiempoMaxNoRefrigerado);
+	  
+
+      
+	 //Servicio5
+	  System.out.println("\nServicio 5: Greedy: Solucion Obtenida");
+	  serv.servicio5(tiempoMaxNoRefrigerado);
+	 
+     
+    }
+ //--------------------------PARA IMPRIMIR------------------------------------------------------------//
+    
+    
+    public static String datosTarea (MyTask tarea) {
+	 	return tarea.getDatosTarea();
+	 }
+    
+    
+    public static String listadoTareas (List<MyTask> lista){
+	 StringBuilder listado = new StringBuilder();
+	 	for (MyTask tarea : lista) {
+            listado.append(tarea.getDatosTarea()).append("\n");
+        }
+
+        return listado.toString();
+	 }
+    
+    
+}
